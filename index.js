9ce48162c84648878d0e64ae57b3a8c837187dca
felipealvx <felipealvk@gmai.com>
2024-03-11 14:47:00 -0300

    animação

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+let typingTexte = new Typed("#text", {
+    strings : ["Designer UX", "Designer UI", "Front-End Developer"],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 50,
+    backDelay : 1000
+})
